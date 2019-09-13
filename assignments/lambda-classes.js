67a48a9a0113b4e7faf6e9b5c38c696407700ab2
Lindsey <FrancesLDow83@gmail.com>
2019-09-12 21:00:40 -0400

    classes almost finished

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,32 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(personAttributes){
+        this.name = personAttributes.name;
+        this.location = personAttributes.location;
+        this.speak = personAttributes.speak(){
+            return `Hello my name is ${this.name}, I am from ${this.location}`;
+        }
+    }
+}
+
+class Instructor extends Person {
+    constructor(personAttributes){
+        super(personAttributes);
+        this.specialty = personAttributes.specialty;
+        this.favLanguage = personAttributes.favLanguage;
+        this.catchPhrase = personAttributes.catchPhrase;
+        this.demo = personAttributes.demo('subject'){
+            console.log(`Today we are learning about ${subject}`);
+        }
+        this.grade = personAttributes.grade(Student,'subject'){
+            console.log(`${Student.name} receives a perfect score on ${subject}`)
+        };
+    }
+}
+
+class Student extends Person {
+    constructor(personAttributes){
+        super(personAttributes)
+        this.previousBackground =
+    }
+}
